9b258588fce64c4c222f78537841029daa0eeb03
Lev Lunev <lunev432@gmail.com>
2022-08-03 12:30:18 +0300

    Switch to local docker-generator

--- build.sh
+++ build.sh
@@ -2,14 +2,7 @@
 set -e
 
 : "${BITCARTGEN_DOCKER_IMAGE:=bitcartcc/docker-compose-generator}"
-if [ "$BITCARTGEN_DOCKER_IMAGE" == "bitcartcc/docker-compose-generator:local" ]; then
-    docker build generator --tag $BITCARTGEN_DOCKER_IMAGE
-else
-    set +e
-    docker pull $BITCARTGEN_DOCKER_IMAGE
-    docker rmi $(docker images bitcartcc/docker-compose-generator --format "{{.Tag}};{{.ID}}" | grep "^<none>" | cut -f2 -d ';') >/dev/null 2>&1
-    set -e
-fi
+docker build generator --tag $BITCARTGEN_DOCKER_IMAGE
 
 docker run -v "$PWD/compose:/app/compose" \
     --env-file <(env | grep BITCART_) \
